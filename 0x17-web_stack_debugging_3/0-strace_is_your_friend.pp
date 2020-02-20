d7e5589bebf9dacdc5c29444e1b85f28c49e7992
dalexach <dalexach@gmail.com>
2020-02-20 03:52:50 +0000

    fixed

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
 # Fixing the issue
 
 exec { 'right path':
-    command => 'sed -i "s/.phpp/.php/g"  /var/www/html/wp-settings.php',
+    cwd => /var/www/html/',
+    command => 'sed -i "s/.phpp/.php/g"  /var/www/html/wp-settings.php'
 }
